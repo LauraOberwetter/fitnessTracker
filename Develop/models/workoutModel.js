2217811ad0ea3e24b63aa4826469c88aad71ba2b
Laura Oberwetter <leoberwetter@gmail.com>
2021-06-08 20:47:52 -0500

    WorkoutScema skeleton created and exported to server.js

--- Develop/models/workoutModel.js
+++ Develop/models/workoutModel.js
@@ -3,5 +3,42 @@ const Schema = mongoose.Schema; //bring in schema class, capture in Schema varia
 
 const WorkoutSchema = new Schema ({ //invoke schema, capture within WorkoutSchema variable
     //define fields
-    
-})
+    type: {
+
+    },
+
+    name: {
+        type: String,
+        trim: true,
+    },
+
+    duration: {
+        type: Number,
+        required: true
+    },
+
+    weight: {
+        type: Number,
+
+    },
+
+    reps: {
+        type: Number,
+
+    },
+
+    sets: {
+        type: Number,
+
+    },
+
+    distance: {
+        type: Number,
+
+    }
+
+});
+
+const Workout = mongoose.model("Workout", WorkoutSchema); // pass WorkoutSchema to model method within Mongoose. This creates a model (Workout) to export
+
+module.exports = Workout; //export model.  imported into server.js 
--- Develop/server.js
+++ Develop/server.js
@@ -3,3 +3,6 @@ const express = require("express");
 
 //build model/connect model file
 const Workout = require("./workoutModel.js"); //require file
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/dbExample", { useNewUrlParser: true }); //UPDATE PATH
+
