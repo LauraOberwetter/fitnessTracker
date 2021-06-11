6d817d2edece88f0dcbfdc56e6804e98a53c1270
Laura Oberwetter <leoberwetter@gmail.com>
2021-06-11 11:14:47 -0400

    routes linked in index.js

---
+++ Develop/controllers/index.js
@@ -0,0 +1,14 @@
+const router = require('express').Router();
+
+//link routes
+const apiRoutes = require('./api');
+const homeRoutes = require('./homeRoutes');
+
+
+
+router.use('/', homeRoutes);
+router.use('/api', apiRoutes);
+
+
+
+module.exports = router;
