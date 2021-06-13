aab55472b8089ea6084975ac497a346e8d7beec5
Laura Oberwetter <leoberwetter@gmail.com>
2021-06-13 17:56:04 -0500

    starting GET workout api route

--- controllers/api/workouts.js
+++ controllers/api/workouts.js
@@ -1,10 +1,22 @@
 const router = require("express").Router();
 const Workout = require("../../models/workoutModel.js"); //require file
 
+
+// GET workout
+router.get("/", (req, res) => { // for home
+  Workout.find({}) //find workout db within workout model
+    .then(Workout => {
+      res.json(Workout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
 //create workout
 //ref from miniproj #26 (transaction)
-router.post("/workout", ({ body }, res) => { //pulling from workoutmodel? or should this be connected to workoutModel.js?
-  Workout.create(body)
+router.post("/", ({ body }, res) => { //pulling from workoutmodel? or should this be connected to workoutModel.js?
+  Workoutdb.create(body)
     .then(dbWorkout => { // does db workout need to be declared first or is this already created with mongoose?
       res.json(dbWorkout);
     })
@@ -23,15 +35,6 @@ router.post("/api/transaction/bulk", ({ body }, res) => {
     });
 });
 
-router.get("/api/transaction", (req, res) => {
-  Transaction.find({})
-    .sort({ date: -1 })
-    .then(dbTransaction => {
-      res.json(dbTransaction);
-    })
-    .catch(err => {
-      res.status(400).json(err);
-    });
-});
+
 
 module.exports = router;
