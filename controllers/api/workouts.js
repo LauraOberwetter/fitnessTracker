ef65566987572e8648ae8a037323a5ed818bb6f5
Laura Oberwetter <leoberwetter@gmail.com>
2021-06-13 21:55:09 -0500

    routes update

--- controllers/api/workouts.js
+++ controllers/api/workouts.js
@@ -16,52 +16,32 @@ router.get("/",  (req, res) => { // for "/"home
 
 
 //PUT exercise in workoutdb
+//updating by id
 //line 17 in api.js ://api/workouts/id
-router.put("/:id", async ({ body, params }, res) => { //api/workouts/id
-  try {
-    
-
-  } catch(err) {
-    res.status(400).json(err);
-  }
-
+router.put("/:id", (req, res, next) => {
+  dbWorkout.collection("type").update({_id: id}, {$set:{'exercise': req.body.exercise}}, (err, result) => {
+    if(err) {
+      throw err;
+    }
+    res.send('exercise updated sucessfully');
+  });
 });
 
 
 //POST submitted workout in the workoutdb, to be pulled later
 //line 29 in api.js: api/workouts
-router.post("/", async (req, res) => { 
-  try {
-
-  } catch(err) {
-    res.status(400).json(err);
-  }
-
+router.post("/", (req, res) => {
+  db.Workout.create(body)
+    .then(({ body }) =>
+      db.Workout.findOneAndUpdate({}, { $push: { exercises: body } })
+    )
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
 });
-
-
-// //create workout
-// //ref from miniproj #26 (transaction)
-// router.post("/", ({ body }, res) => { //pulling from workoutmodel? or should this be connected to workoutModel.js?
-//   Workout.create(body)
-//     .then(dbWorkout => { // does db workout need to be declared first or is this already created with mongoose?
-//       res.json(dbWorkout);
-//     })
-//     .catch(err => { // error return
-//       res.status(400).json(err);
-//     });
-// });
-
-// router.post("/api/transaction/bulk", ({ body }, res) => {
-//   Transaction.insertMany(body)
-//     .then(dbTransaction => {
-//       res.json(dbTransaction);
-//     })
-//     .catch(err => {
-//       res.status(400).json(err);
-//     });
-// });
-
 
 
 module.exports = router;
